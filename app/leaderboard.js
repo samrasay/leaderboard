8f9daf9d68374d45addb7a0871143fe614715ea0
samrasay <samrasay@dhcp-168-105-30-159.wireless.manoa.hawaii.edu>
2017-02-28 10:07:12 -1000

    Task 5: Events, Sessions

--- app/leaderboard.js
+++ app/leaderboard.js
@@ -1,1 +1,29 @@
+PlayersList = new Mongo.Collection('players');
 console.log("Hello world!");
+
+if(Meteor.isClient){
+  Template.leaderboard.helpers({
+    'player': function(){
+      return PlayersList.find();
+    },
+    'selectedClass': function(){
+      //return "selected";
+      var playerId = this._id;
+      var selectedPlayer = Session.get('selectedPlayer');
+      if(playerId == selectedPlayer){
+        return "selected"
+      }
+    }
+  })
+
+  Template.leaderboard.events({
+    'click .player': function(){
+      var playerId = this._id;
+      Session.set('selectedPlayer', playerId);
+    }
+  })
+
+}
+
+if(Meteor.isServer){
+}
